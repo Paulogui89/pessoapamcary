fd7f391faef63eb2e9ca8712174ea60fb269e39c
Paulogui89 <paulo_phs89@hotmail.com>
2022-04-24 10:25:29 -0300

    estrutura inicial e configuracao db

---
+++ src/main/java/com/gpspamcary/pessoapamcary/PessoapamcaryApplication.java
@@ -0,0 +1,13 @@
+package com.gpspamcary.pessoapamcary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoapamcaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoapamcaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gpspamcary/pessoapamcary/PessoapamcaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpspamcary.pessoapamcary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoapamcaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
